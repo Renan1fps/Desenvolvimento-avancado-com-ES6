573c4be84cba08ede8fd090525224abcfebb3530
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-20 19:13:32 -0300

    Tuple

---
+++ typescript/src/tipagem/tuple.ts
@@ -0,0 +1,10 @@
+/*Com tuple posso definir quais dados eu quero e o tamanho dentro de um array */
+
+let dados: [string, number, boolean];
+/*Assim eu determinei que meu array terá um tamanho de três elementos e tenho que seguir essa ordem de inserção (string, number e boolean)*/
+
+dados = ["Renan", 21, true];
+
+/*Caso tente colocar a mais ou fora dessa ordem ele dará erro */
+
+
