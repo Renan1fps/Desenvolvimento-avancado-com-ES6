12f829f830aae6a9b087438a1a64e7ca610be583
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-20 18:56:07 -0300

    Arrays

---
+++ typescript/src/tipagem/arrays.ts
@@ -0,0 +1,7 @@
+const filmesCategorias: string[] = ["comédia", "drama", "ação"];
+
+filmesCategorias.push("aventura");
+
+const senhas: Array<string> = ["f45f5", "sdxa88", "fwefc88"];
+
+const keyPix: (string | number)[] = ["d1f851", 202566, "email@gmail.com"];
