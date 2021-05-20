3b86af33e26509092d6358308e9af4ba5018968d
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-20 15:51:44 -0300

    Compilando typescript para diferentes versões do EMCA

---
+++ typescript/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello world!");
+var Produto = /** @class */ (function () {
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    return Produto;
+}());
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,13 @@
 console.log("Hello world!");
+
+class Produto {
+  nome: string;
+  valor: number;
+
+  constructor(produtoNome: string, produtoValor: number) {
+    this.nome = produtoNome;
+    this.valor = produtoValor;
+  }
+}
+
+const leite = new Produto("leite", 20);
