24513c5738f7110ef0e8fd6223b72e82bc3d97e8
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-20 17:10:37 -0300

    Union types

--- typescript/build/index.js
+++ typescript/build/index.js
@@ -1,9 +1,10 @@
 "use strict";
 console.log("Hello world!");
-var Produto = /** @class */ (function () {
+var Produto = (function () {
     function Produto(produtoNome, produtoValor) {
         this.nome = produtoNome;
         this.valor = produtoValor;
     }
     return Produto;
 }());
+var leite = new Produto("leite", 20);
---
+++ typescript/src/tipagem/unionTypes.ts
@@ -0,0 +1,7 @@
+let chavePix: string | number;
+
+chavePix = "renan@gmail.com";
+
+chavePix = 958315;
+
+/*Aceita os valores que eu declarei na inicialização */
