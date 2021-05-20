5d7bc2061e8c8a787ccb2fdc48e6d90f3a51fc6f
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-20 20:13:13 -0300

    Void

---
+++ typescript/src/tipagem/void.ts
@@ -0,0 +1,6 @@
+function mostrarFrase(): void {
+  console.log("Imprime frase!");
+}
+
+
+/*usado mais em funções que não irão retornar nada (função sem o return) que apenas servem para executar uma função*/
