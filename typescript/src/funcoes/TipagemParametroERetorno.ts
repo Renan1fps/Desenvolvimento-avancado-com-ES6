3d9238a4235f16817d776f6aea6ced9c0fc864f6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-21 14:55:26 -0300

    Tipagem de funções

---
+++ typescript/src/funcoes/TipagemParametroERetorno.ts
@@ -0,0 +1,14 @@
+function showMessage(texto: string): string {
+  return texto;
+}
+
+function showMessage2(texto: string, texto2: string): string {
+  return texto + texto2;
+}
+
+const arrow = (soma: number): number => soma + 1;
+
+function anyReturns(valor: string | number): string | number {
+  return valor;
+}
+
