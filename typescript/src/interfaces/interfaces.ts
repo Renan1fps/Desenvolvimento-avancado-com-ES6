8febe5950a1048cf4d33d3125e09b6daac8d6576
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-21 15:54:51 -0300

    Interfaces

---
+++ typescript/src/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+interface IProduto {
+  nome: string;
+  valor: number;
+  marca: string;
+  lote: string | number;
+}
+let produto: IProduto;
+
+produto = {
+  lote: "2558s45d",
+  marca: "marca Nova",
+  nome: "sabonete",
+  valor: 25.56,
+};
+
+/*Padrão para interfaces: começar com I e em seguinda o nome da classe que irá implementa-lá ex: classe animal --> interface IAnimal */
