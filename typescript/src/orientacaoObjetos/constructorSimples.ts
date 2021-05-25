142a06d30542be1cf7d8b70096cb77287b1d7bd6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-25 14:26:41 -0300

    Constructor simples

---
+++ typescript/src/orientacaoObjetos/constructorSimples.ts
@@ -0,0 +1,9 @@
+class ConstructorSimples {
+  constructor(
+    private nome: string,
+    private senha: string,
+    private idade: number
+  ) {}
+}
+
+const construcutorSimples = new ConstructorSimples("nome", "senha", 10);
