d121f790d812a2b598f28c8a22c0a1cb5d7d1810
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-26 14:46:36 -0300

    Exports

--- typescript/src/exportDeModulos/app/TesteConexaoDB.ts
+++ typescript/src/exportDeModulos/app/TesteConexaoDB.ts
@@ -3,3 +3,5 @@ import { BancoDeDados } from "../classes/BancoDeDados";
 const Conectar = BancoDeDados.Conectar();
 
 console.log(Conectar);
+
+/*export default é quando vou exportar apenas uma coisa, e export diretamente é quando vou exportar varias coisas. */
